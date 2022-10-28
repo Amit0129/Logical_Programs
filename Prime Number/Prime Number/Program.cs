690b0f87dce3a85ab5d1be0b48f0f76d8ca5ae41
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-28 17:52:15 +0530

    Adding: Prime Number Program

---
+++ Prime Number/Prime Number/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Prime_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number,count=0,i;
+            Console.Write("Enter a number");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("The number {0} is a Prime Number", number);
+            }
+            else
+            {
+
+                Console.WriteLine("The number {0} is not a Prime Number", number);
+            }
+          
+
+        }
+    }
+}
