b667cd7c8eb9aa63c4a105e009da91bd066d5fb1
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-28 18:25:31 +0530

    Added: Simulate Stopwatch Program

---
+++ Simulate Stopwatch Program/Simulate Stopwatch Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace Simulate_Stopwatch_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopW = new Stopwatch();
+            stopW.Start();
+            Thread.Sleep(5000);
+            stopW.Stop();
+            TimeSpan tspan = stopW.Elapsed;
+            string elapsedTime = String.Format(" {0:00}:{1:00}:{2:00}.{3:00} ",
+            tspan.Hours, tspan.Minutes, tspan.Seconds, tspan.Milliseconds / 10);
+            Console.WriteLine(" RunTime " + elapsedTime);
+            
+        }
+    }
+}
