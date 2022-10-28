d76a6eb8e7f7dc1a1b64f3fba3e6e418b29533e1
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-28 18:08:24 +0530

    Added Coupon Numbers program

---
+++ Coupon Numbers/Coupon Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Coupon_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the copun code");
+            int len = Convert.ToInt32(Console.ReadLine());
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[len];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            Console.WriteLine(finalString);
+        }
+    }
+}
