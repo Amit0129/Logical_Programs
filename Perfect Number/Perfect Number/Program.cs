44b3a33f9b673eb276952d35be2ba4ab5486d3a3
Amit0129 <amitkumarnayak40@gmail.com>
2022-10-28 17:49:10 +0530

    Adding:Perfect Number Program

---
+++ Perfect Number/Perfect Number/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Perfect_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, sum = 0, number;
+            Console.Write("Enter the number wamt to check: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            for (i=1; i<number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum+=i;
+                }
+            }
+            if (number == sum)
+            {
+                Console.WriteLine("The number {0} is a perfect number",number);
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is not a perfect number", number);
+            }
+        }
+    }
+}
